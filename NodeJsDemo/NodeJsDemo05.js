a5624cc42a26d6b9524a57504cb04aec474dd27a
zhengjin <zivieira@163.com>
2016-11-10 00:02:54 +0800

    Add a demo, handle input and output by using stream.

--- NodeJsDemo/NodeJsDemo05.js
+++ NodeJsDemo/NodeJsDemo05.js
@@ -3,11 +3,27 @@
  *
  * Read file data refer to Node.js Tutorial.
  */
-var fs = require('fs');
+var fs = require('fs'),
+    http = require('http');
 
-var stream = fs.readFile('./NodeJsDemo/data.txt', 'utf8', function (error, data) {
+// use file
+fs.readFile('./data.txt', 'utf8', function (error, data) {
     if (error) {
-        console.log(error.message);
+        console.error(error.message);
     }
     console.log(data);
+});
+
+// use stream
+var server = http.createServer(function (req, res) {
+    var stream = fs.createReadStream('data.txt');
+    stream.on('error', function (error) {
+        res.status = 500;
+        res.end(String(error));
+    });
+    stream.pipe(res);
+});
+
+server.listen(3000, function () {
+   console.log('Server is listening on port 3000.');
 });
