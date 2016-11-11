2d2adb97a93fa366ef71cb9f6773c5b71b577327
Vieira-zj <zhengjin@fun.tv>
2016-11-11 20:24:57 +0800

    Add demo for generator.

--- JsDemo/JsDemo02.js
+++ JsDemo/JsDemo02.js
@@ -124,3 +124,78 @@
 //var thing = new Thing("fruit");
 //thing.logThings(['apples','orange','banana']);
 
+
+// demo 08, Generator
+// method 1: function
+//function fib(max) {
+//    var t,
+//        a = 0,
+//        b = 1,
+//        arr = [0, 1];
+//
+//    while (arr.length < max) {
+//        t = a + b;
+//        a = b;
+//        b = t;
+//        arr.push(t);
+//    }
+//
+//    return arr;
+//}
+//
+//console.log(fib(10));
+
+// method 2: generator
+//function* fib(max) {
+//    var t,
+//        a = 0,
+//        b = 1,
+//        n = 1;
+//
+//    while (n < max) {
+//        yield a;
+//        t = a + b;
+//        a = b;
+//        b = t;
+//        n++;
+//    }
+//
+//    return a;
+//}
+//
+//var fn = fib(5);
+//console.log(fn.next());
+//console.log(fn.next());
+//console.log(fn.next());
+//console.log(fn.next());
+//console.log(fn.next());
+
+//for (var x of fib(5)) {
+//    console.log(x);
+//}
+
+// method 3: object, keep status via properties
+//var fib = {
+//    a: 0,
+//    b: 1,
+//    n: 0,
+//    max: 5,
+//    next: function () {
+//        var r = this.a,
+//            t = this.a + this.b;
+//        this.a = this.b;
+//        this.b = t;
+//        if (this.n < this.max) {
+//            this.n++;
+//            return r;
+//        } else {
+//            return undefined;
+//        }
+//    }
+//};
+//
+//console.log(fib.next());
+//console.log(fib.next());
+//console.log(fib.next());
+//console.log(fib.next());
+//console.log(fib.next());
