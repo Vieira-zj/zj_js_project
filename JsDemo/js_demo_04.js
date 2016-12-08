73910e79fc8c051cbafdd01c3831a818dfa5eeb7
Vieira-zj <zhengjin@fun.tv>
2016-12-08 17:05:25 +0800

    Add script to verify the weather ret data from weather.com.cn.

---
+++ JsDemo/js_demo_04.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zhengjin on 2016/12/8.
+ */
+// demo 01, date
+//var myDate = new Date();
+//console.log('Current date: %d-%d-%d', myDate.getFullYear(), myDate.getMonth()+1, myDate.getDate());
+
+console.log(__filename, 'DONE.');
