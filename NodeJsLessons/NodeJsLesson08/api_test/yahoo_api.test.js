30dfc9927733bd4b5e6567394353048790d0c1f5
Vieira-zj <zhengjin@fun.tv>
2016-12-13 11:52:18 +0800

    Add script to verify yahoo weather api.

--- NodeJsLessons/NodeJsLesson08/api_test/yahoo_api.test.js
+++ NodeJsLessons/NodeJsLesson08/api_test/yahoo_api.test.js
@@ -7,12 +7,20 @@
 var agent = require('superagent');
 var should = require('should');
 
+var buildUrl = function (cityName) {
+    var yahooUrl = 'https://query.yahooapis.com/v1/public/yql?';
+    var yahooYql = 'q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'
+        + cityName + '%22)';
+    var yahooFormat = '&format=json';
+
+    return yahooUrl + yahooYql + yahooFormat;
+};
+
 describe('Test weather data from YaHoo', function () {
     var respContent;
 
     before('', function (done) {
-        var url = 'TODO';
-        agent.get(url).end((err, content) => {
+        agent.get(buildUrl('BeiJing')).end((err, content) => {
             if (err) {
                 console.error(err.message);
             }
