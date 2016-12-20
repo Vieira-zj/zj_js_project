8477d761c8422a171ccc71d75693c1020b87099a
vieira <zivieira@163.com>
2016-12-20 16:05:56 +0800

    Add demos.

--- JsDemo/js_demo_04.js
+++ JsDemo/js_demo_04.js
@@ -6,5 +6,37 @@
 //console.log('cur date: %d-%d-%d', myDate.getFullYear(), myDate.getMonth()+1, myDate.getDate());
 //console.log('cur date:', [myDate.getFullYear(), myDate.getMonth()+1, myDate.getDate()].join('-'));
 
+//console.log(myDate.getDate());
+//console.log(myDate.getDay());
+
+
+// demo 02, convert
+//var tmpInt = 5;
+//var tmpStr = String(tmpInt);
+//console.log(typeof tmpStr);
+
+//var tmpStr = '5';
+//var tmpInt = parseInt(tmpStr);
+//console.log(typeof tmpInt);
+
+//var tmpStr = '';
+//console.log(tmpStr.length);
+
+
+// demo 03, expr
+//var tmpStr = '低温 -2℃';
+//var re = /(-?\d+)℃/;
+//console.log(re.exec(tmpStr));
+
+
+// demo 04, exit
+//var tmpFlag = false;
+//if (tmpFlag) {
+//    console.log('true');
+//} else {
+//    console.log('false and return');
+//    return;
+//}
+
 
 console.log(__filename, 'DONE.');
