143b1a002bf38f3b5ee6c283a0d4f5d20132b5c6
vieira <zivieira@163.com>
2017-04-14 10:28:32 +0800

    Add js demos using underscore and lodash.

---
+++ NodeJsLessons/NodeJsLesson01/demo_lodash.js
@@ -0,0 +1,24 @@
+/**
+ * Created by zhengjin on 2017/4/14.
+ */
+
+var _ = require('lodash');
+
+if (require.main === module) {
+    var users = [
+        { 'user': 'barney', 'age': 36 },
+        { 'user': 'fred', 'age': 40 },
+        { 'user': 'pebbles', 'age': 1 }
+    ];
+
+    var youngest = _.chain(users)
+        .sortBy('age')
+        .map(function (chr) {
+            return chr.user + ' is ' + chr.age;
+        })
+        .first()
+        .value();
+    console.log('youngest:', youngest);
+
+    console.log(__filename, 'Done!');
+}
