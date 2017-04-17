caa2fa81977fec1ae103b4d39aabb3276cf7f940
zhengjin <zivieira@163.com>
2017-04-18 01:06:28 +0800

    Add js demos.

--- NodeJsLessons/JsJquerySample/app.js
+++ NodeJsLessons/JsJquerySample/app.js
@@ -7,12 +7,16 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 
+var getTextOfDomObject = function (domObject) {
+    return domObject.children[0].data;
+};
+
 var testFindById = function ($) {
     var tmpDiv = $('#div_test1');
 
     // jquery object => dom object
     var divDom = tmpDiv.get(0);
-    console.log('text from dom object:', divDom.children[0].data);
+    console.log('text from dom object:', getTextOfDomObject(divDom));
 
     // dom object => jquery object
     var divJqObject = $(divDom);
@@ -21,24 +25,24 @@ var testFindById = function ($) {
 
 var testFindByClass = function ($) {
     var tmpDiv = $('div.green');
-    console.log('text:', tmpDiv.text());
+    console.log('inner html:', tmpDiv.html());
 };
 
 var testFindByLevels = function ($) {
     // parent => children
     var tmpLiJs1 = $('div#div_test2 li.lang-javascript');
-    console.log('text:', tmpLiJs1.text());
+    console.log('child text:', tmpLiJs1.text());
 
-    // parent => direct children
+    // parent => 1 level children
     var tmpLiJs2 = $('ul.lang>li.lang-javascript');
-    console.log('text:', tmpLiJs2.text());
+    console.log('child text:', tmpLiJs2.text());
 
     // filter
     var tmpLiLua = $('div#div_test2>ul.lang>li:last-child');
-    console.log('text:', tmpLiLua.text());
+    console.log('child text:', tmpLiLua.text());
 
     var tmpLiPy = $('div#div_test2>ul.lang>li:nth-child(2)');
-    console.log('text:', tmpLiPy.text());
+    console.log('child text:', tmpLiPy.text());
 };
 
 var testFindFromJqObject = function ($) {
@@ -46,39 +50,57 @@ var testFindFromJqObject = function ($) {
 
     // find()
     var swf = tmpLangUl.find('#swift');
-    console.log('text:', swf.text());
+    console.log('language text:', swf.text());
 
     var hsk = tmpLangUl.find('[name=haskell]');
-    console.log('text:', hsk.text());
+    console.log('language text:', hsk.text());
 
     // parent()
     var js = hsk.parent().find('.js');
-    console.log('text:', js.text());
+    console.log('language text:', js.text());
 
     // next() and prev()
     var sch = swf.next();
-    console.log('text:', sch.text());
+    console.log('language text:', sch.text());
     var py = swf.prev();
-    console.log('text:', py.text());
+    console.log('language text:', py.text());
 };
 
 var testFilterFromJqObject = function ($) {
     var tmpLangLis = $('div#div_test3>ul.lang>li');
 
+    // each
+    tmpLangLis.each(function () {
+        console.log('language:', getTextOfDomObject(this));
+    });
+
+    tmpLangLis.each(function (idx, element) {
+        console.log('language:', $(element).text(), 'at position:', idx);
+    });
+
     // filter
     var swf = tmpLangLis.filter('#swift');
-    console.log('text:', swf.text());
+    console.log('filter language:', swf.text());
 
     var tmpLangs = tmpLangLis.filter(function () {
-        return this.children[0].data.indexOf('S') === 0;
+        return getTextOfDomObject(this).indexOf('S') === 0;
     });
-    console.log(tmpLangs.first().text());
+    console.log('language start with S at 1st:', tmpLangs.first().text());
+    console.log('language start with S at 2nd:', getTextOfDomObject(tmpLangs.get(1)));
 
     // map
     var tmpArr = tmpLangLis.map(function () {
-        return this.children[0].data;
+        return $(this).text();
     }).get();
     console.log('Languages:', tmpArr);
+};
+
+var testGetJqObjectAttr = function ($) {
+    var tmpLangs = $('div#div_test3>ul>li');
+    var tmpLang = tmpLangs.filter(function () {
+        return getTextOfDomObject(this) === 'Haskell';
+    });
+    console.log('Haskell name attribute value:', tmpLang.attr('name'));
 };
 
 
@@ -92,6 +114,7 @@ if (require.main === module) {
     testFindByLevels($);
     testFindFromJqObject($);
     testFilterFromJqObject($);
+    testGetJqObjectAttr($);
 
     console.log('JQuery demo done!');
 }
