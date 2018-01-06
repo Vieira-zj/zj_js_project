e90919d0e60143c44564b490b5c4f34525be62e5
zhengjin <zivieira@163.com>
2018-01-06 23:30:25 +0800

    Create vue demo.

--- VueDemo/src/main.js
+++ VueDemo/src/main.js
@@ -1,15 +1,42 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// main.js
+
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+import Home from './pages/Home'
+import Detail from './pages/Detail'
 
-/* eslint-disable no-new */
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/detail',
+    component: Detail
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+// eslint-disable-next-line
 new Vue({
   el: '#app',
+  data () {
+    return {
+      transitionName: 'slide'
+    }
+  },
   router,
-  template: '<App/>',
-  components: { App }
+  watch: {
+    '$route' (to, from) {
+      const toDepth = to.path.substring(0, to.path.length - 2).split('/').length
+      const fromDepth = from.path.substring(0, from.path.length - 2).split('/').length
+      this.transitionName = toDepth < fromDepth ? 'slide_back' : 'slide'
+    }
+  }
 })
