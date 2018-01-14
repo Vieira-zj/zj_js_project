57e7c451450bb65f732076663313038d65f73d7b
zhengjin <zivieira@163.com>
2018-01-14 21:38:30 +0800

    Refactor project.

--- nodejs_lessons/nodejs_lesson_08/app.js
+++ nodejs_lessons/nodejs_lesson_08/app.js
@@ -4,15 +4,15 @@
  * Async invoke by using bluebird.
  */
 
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
+var Blpromise = require('bluebird');
+var fs = Blpromise.promisifyAll(require('fs'));
 
 fs.readFileAsync('./data.txt', 'utf8').then(function (content) {
     console.log('Success read async, content: ');
     console.log(content);
 }).catch(SyntaxError, function (e) {
     console.error(e.message);
-}).catch(Promise.OperationalError, function (e) {
+}).catch(Blpromise.OperationalError, function (e) {
     console.error("unable to read file, because: ", e.message);
 }).catch(reason => console.log(reason));
 
