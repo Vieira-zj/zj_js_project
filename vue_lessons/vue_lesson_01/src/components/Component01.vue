db6d0006c1af1a26a6e77459c37431a43a1d8edd
zhengjin <zivieira@163.com>
2018-01-16 10:35:38 +0800

    Update demo.

---
+++ vue_lessons/vue_lesson_01/src/components/Component01.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="component-01">
+    <h1>My First Component</h1>
+    <span>{{ message }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Component01',
+  data () {
+    return {
+      message: 'My hello world vue.js!'
+    }
+  }
+}
+</script>
