e97ae21216f49f6a19a49ac2c12a14d660a62f0f
zhengjin <zivieira@163.com>
2018-06-09 11:16:21 +0800

    refactor.

--- vue_lessons/vue_lesson_02/src/components/lessons/Lesson05.vue
+++ vue_lessons/vue_lesson_02/src/components/lessons/Lesson05.vue
@@ -1,33 +1,33 @@
 <template>
   <div id="lesson-05">
     <h1>Lesson 05</h1>
-    <div id="example 01" v-if="seen1">
+    <div id="example-01" v-if="seen1">
       <h2>Example 01: v-if and v-else</h2>
       <div v-if="Math.random() > 0.5">Now you see me</div>
       <div v-else>Now you don't</div>
     </div>
-    <div id="example 02" v-if="seen2">
+    <div id="example-02" v-if="seen2">
       <h2>Example 02: v-show</h2>
       <p v-show="ok">v-show only toggles the display CSS property of the element.</p>
     </div>
-    <div id="example 03" v-if="seen3">
-      <h2>Example 03: list render</h2>
+    <div id="example-03" v-if="seen3">
+      <h2>Example 03: v-for, list render</h2>
       <ul>
         <li v-for="(item, index) in items" v-bind:key="index">
           {{ parentMessage }} - {{ index }} - {{ item.message }}
         </li>
       </ul>
     </div>
-    <div id="example 04" v-if="seen4">
-      <h2>Example 04: v-for an object</h2>
+    <div id="example-04" v-if="seen4">
+      <h2>Example 04: v-for, list an object</h2>
       <ul>
         <li v-for="(v, k) in object" v-bind:key="k">
           {{ k }} : {{ v }}
         </li>
       </ul>
     </div>
-    <div id="example 05" v-if="seen5">
-      <h2>Example 05, display filter results</h2>
+    <div id="example-05" v-if="seen5">
+      <h2>Example 05, v-for, display filter results</h2>
       <div id="computed-property">
         <ul>
           <li v-for="(n, idx) in evenNumbers" v-bind:key="idx">
@@ -43,13 +43,39 @@
         </ul>
       </div>
     </div>
-    <div id="example 06" v-if="seen6">
+    <div id="example-06" v-if="seen6">
       <h2>Example 06, v-for with v-if</h2>
       <ul v-if="todos.length">
         <li v-for="(todo, idx) in todos" v-if="todo.isComplete" v-bind:key="idx">
           {{ todo.lang }} completed
         </li>
       </ul>
+    </div>
+    <div id="example-07" v-if="seen7">
+      <h2>Example 07, v-if and v-else with shared element</h2>
+      <div id="example-0701">
+        <p><b>Example 07-01, shared input element</b></p>
+        <div v-if="loginType === 'username'">
+          <label>Username</label>
+          <input placeholder="Enter your username">
+        </div>
+        <div v-else>
+          <label>Email</label>
+          <input placeholder="Enter your email address">
+        </div>
+      </div>
+      <div id="example-0702">
+        <p><b>Example 07-02, non-shared input element by key</b></p>
+        <div v-if="loginType === 'username'">
+          <label>Username</label>
+          <input placeholder="Enter your username" key="username-input">
+        </div>
+        <div v-else>
+          <label>Email</label>
+          <input placeholder="Enter your email address" key="email-input">
+        </div>
+      </div>
+      <button v-on:click="setLoginType">Toogle login type</button>
     </div>
   </div>
 </template>
@@ -59,7 +85,7 @@ export default {
   name: 'Lesson05',
   data () {
     return {
-      seen1: true,
+      seen1: false,
       seen2: true,
       ok: true,
       seen3: true,
@@ -82,12 +108,21 @@ export default {
         {lang: 'JavaScript', isComplete: false},
         {lang: 'Python', isComplete: true},
         {lang: 'Golang', isComplete: false}
-      ]
+      ],
+      seen7: false,
+      loginType: 'username'
     }
   },
   methods: {
     notEven: function () {
       return this.numbers.filter((number) => number % 2 !== 0)
+    },
+    setLoginType: function () {
+      if (this.loginType.toLowerCase().indexOf('email') >= 0) {
+        this.loginType = 'username'
+      } else {
+        this.loginType = 'useremails'
+      }
     }
   },
   computed: {
--- vue_lessons/vue_lesson_02/src/components/lessons/Lesson06.vue
+++ vue_lessons/vue_lesson_02/src/components/lessons/Lesson06.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="lesson-06">
     <h1>Lesson 06</h1>
-    <div id="example 01" v-show="seen1">
+    <div id="example-01" v-show="seen1">
       <h2>Example 01: listening to event</h2>
       <button v-on:click="counter += 1">Add 1</button>
       <p>The button above has been clicked {{ counter }} times.</p>
     </div>
-    <div id="example 02" v-show="seen2">
+    <div id="example-02" v-show="seen2">
       <h2>Example 02: method event handler</h2>
       <button v-on:click="greet">Greet</button><br>
-      <button v-on:click="warn('Form cannot be submitted yet.', $event)">Sbumit</button>
+      <button v-on:click="showWarn('Form cannot be submitted yet.', $event)">Sbumit</button>
     </div>
-    <div id="example 03" v-show="seen3">
+    <div id="example-03" v-show="seen3">
       <h2>Example 03: method in inline handler</h2>
       <button v-on:click="say('what')">Say what</button><br>
       <button v-on:click="say('hi')">Say hi</button>
     </div>
-    <div id="example 04" v-show="seen4">
+    <div id="example-04" v-show="seen4">
       <h2>Example 04: event modifiers - self</h2>
         <div v-on:click.self="say('div on.click.self')">
           Div on.click.self
@@ -25,20 +25,19 @@
           </div>
         </div>
     </div>
-    <div id="example 05" v-show="seen5">
-      <h2>Example 05: event modifiers - prevent</h2>
+    <div id="example-05" v-show="seen5">
+      <h2>Example 05: event modifiers - stop</h2>
         <!-- prevent? -->
         <div v-on:click="say('parent on.click')">
           Parent on.click
-          <div v-on:click.prevent="say('child on.click')">
+          <div v-on:click.stop="say('child on.click')">
             <button>Click me</button>
           </div>
         </div>
     </div>
-    <div id="example 06" v-show="seen6">
+    <div id="example-06" v-show="seen6">
       <h2>Example 06: key modifiers</h2>
-      <!-- ctrl+c: clear text -->
-      <input type="text" v-model="inputText"
+      <input type="text" v-model="inputText" placeholder="ctrl+c: clear text"
         v-on:keyup.enter="say(inputText + ' submitted')"
         v-on:keyup.ctrl.67="clearText()">
     </div>
@@ -55,7 +54,7 @@ export default {
       seen2: true,
       message: 'Vue.js',
       seen3: false,
-      seen4: false,
+      seen4: true,
       seen5: true,
       seen6: true,
       inputText: ''
@@ -65,17 +64,17 @@ export default {
     greet: function (event) {
       alert('Hello ' + this.message + '!')
       if (event) {
-        alert(event.target.tagName)
+        alert('target tag => ' + event.target.tagName)
       }
     },
-    say: function (msg) {
-      alert(msg)
-    },
-    warn: function (text, event) {
+    showWarn: function (text, event) {
       if (event) {
         event.preventDefault()
       }
       alert(text)
+    },
+    say: function (msg) {
+      alert('say: ' + msg)
     },
     clearText: function () {
       this.inputText = ''
