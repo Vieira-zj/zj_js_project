efd40ea85c978bb3cd8a1933712573b3142c1728
Zheng Jin <zivieira@163.com>
2024-07-17 19:15:27 +0800

    Add utils for ts web app.

--- ts_apps/web/src/utils/fs.ts
+++ ts_apps/web/src/utils/fs.ts
@@ -3,3 +3,16 @@ import path from 'path'
 export const getRootPath = (): string => {
   return path.dirname(path.dirname(__dirname))
 }
+
+export const safeJsonParse = (obj?: string): any | undefined => {
+  if (!obj) {
+    return undefined
+  }
+
+  try {
+    return JSON.parse(obj)
+  } catch (e: any) {
+    console.error('json parse fail:', e)
+    return undefined
+  }
+}
